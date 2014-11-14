dc52a23cf280fc5dfab3a70ed86ae01331bea71a
Jodie Alaine Parker <me@jodiealaine.com>
2014-11-14 16:19:41 +0000

    Add nested resources for contact details

--- router.js
+++ router.js
@@ -1,4 +1,8 @@
 Blogger.Router.map(function(){
-	this.resource('posts');
+	this.resource('posts', {path: '/'});
 	this.resource('about');
+	this.resource('contact', function() {
+		this.resource('phone');
+		this.resource('email');
+	});
 })
